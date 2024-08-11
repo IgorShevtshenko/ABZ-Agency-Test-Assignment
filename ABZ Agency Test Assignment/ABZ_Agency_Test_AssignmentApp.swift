a5816e6d55c7368d32f9248eb37753cb6d27d21e
IgorShevtshenko <igorse2001@gmail.com>
2024-08-11 15:31:11 +0300

    Setup project

---
+++ ABZ Agency Test Assignment/ABZ_Agency_Test_AssignmentApp.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+import UILibrary
+
+@main
+struct ABZ_Agency_Test_AssignmentApp: App {
+    
+    init() {
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
