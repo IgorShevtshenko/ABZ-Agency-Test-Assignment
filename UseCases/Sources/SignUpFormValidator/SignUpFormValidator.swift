483365c03bb3789ba426339f1cb4e14bd862613e
IgorShevtshenko <igorse2001@gmail.com>
2024-08-13 21:01:38 +0300

    Refactor SignUpFormValidator

--- UseCases/Sources/SignUpFormValidator/SignUpFormValidator.swift
+++ UseCases/Sources/SignUpFormValidator/SignUpFormValidator.swift
@@ -4,30 +4,7 @@ public enum SignUpFormField: CaseIterable {
     case email
 }
 
-public struct SignUpForm {
-    
-    public let name: String
-    public let phone: String
-    public let email: String
-    
-    public init(name: String, phone: String, email: String) {
-        self.name = name
-        self.phone = phone
-        self.email = email
-    }
-}
-
-public struct SignUpFormFieldValidationResult {
-    
-    public let field: SignUpFormField
-    public let error: String?
-    
-    public init(field: SignUpFormField, error: String?) {
-        self.field = field
-        self.error = error
-    }
-}
-
 public protocol SignUpFormValidator {
-    func validate(_ form: SignUpForm) -> [SignUpFormFieldValidationResult]
+    
+    func validate(field: SignUpFormField, for input: String) -> String?
 }
--- UseCases/Sources/SignUpFormValidatorImpl/SignUpFormValidatorImpl.swift
+++ UseCases/Sources/SignUpFormValidatorImpl/SignUpFormValidatorImpl.swift
@@ -1,81 +1,45 @@
 import SignUpFormValidator
 import PhoneNumberService
+import Foundation
 
 public struct SignUpFormValidatorImpl: SignUpFormValidator {
     
-    private let emailValidation: EmailValidation
     private let phoneNumberService: PhoneNumberService
     
     public init(phoneNumberService: PhoneNumberService) {
-        self.emailValidation = EmailValidation()
         self.phoneNumberService = phoneNumberService
     }
     
-    public func validate(_ form: SignUpForm) -> [SignUpFormFieldValidationResult] {
-        SignUpFormField.allCases
-            .reduce(into: [SignUpFormFieldValidationResult]()) { result, field in
-                let fieldValue = form.value(for: field)
-                if let isRequiredValidation = validateForAbsence(
-                    field: field,
-                    value: fieldValue
-                ) {
-                    result.append(isRequiredValidation)
-                }
-                switch field {
-                case .name:
-                    result.append(validateName(fieldValue))
-                case .phone:
-                    result.append(validatePhone(fieldValue))
-                case .email:
-                    result.append(validateEmail(fieldValue))
-                }
+    public func validate(field: SignUpFormField, for input: String) -> String? {
+        if let isRequiredValidation = validateForAbsence(value: input) {
+            return isRequiredValidation
+        }
+        switch field {
+        case .name:
+            return validateName(input)
+        case .phone:
+            return validatePhone(input)
+        case .email:
+            return validateEmail(input)
         }
     }
     
     private func validateForAbsence(
-        field: SignUpFormField,
         value: String
-    ) -> SignUpFormFieldValidationResult? {
-        guard !value.isEmpty else {
-            return SignUpFormFieldValidationResult(
-                field: field,
-                error: "Required field"
-            )
-        }
-        return nil
+    ) -> String? {
+        !value.isEmpty ? nil : "Required field"
     }
-    private func validateName(_ name: String) -> SignUpFormFieldValidationResult {
-        SignUpFormFieldValidationResult(
-            field: .name,
-            error: (2..<60).contains(name.count) ? nil : "Username should contain 2-60 characters"
-        )
+    private func validateName(_ name: String) -> String? {
+        (2...60).contains(name.count) ? nil : "Username should contain 2-60 characters"
     }
     
-    private func validatePhone(_ phone: String) -> SignUpFormFieldValidationResult {
-        SignUpFormFieldValidationResult(
-            field: .phone,
-            error: phoneNumberService.isValid(phone) ? nil : "Invalid phone number"
-        )
+    private func validatePhone(_ phone: String) -> String? {
+        phoneNumberService.isValid(phone) ? nil : "Invalid phone number"
     }
     
-    private func validateEmail(_ email: String) -> SignUpFormFieldValidationResult {
-        SignUpFormFieldValidationResult(
-            field: .email,
-            error: emailValidation.isValid(email) ? nil : "Invalid email"
-        )
-    }
-}
-
-private extension SignUpForm {
-    
-    func value(for field: SignUpFormField) -> String {
-        switch field {
-        case .name:
-            name
-        case .phone:
-            phone
-        case .email:
-            email
-        }
+    private func validateEmail(_ email: String) -> String? {
+        let emailRegex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,64}$"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES[c] %@", emailRegex)
+        return emailPredicate.evaluate(with: email) ? nil : "Invalid email"
     }
 }
